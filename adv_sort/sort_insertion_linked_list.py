144cba91a56225d43d09700460a1df1fba01f6e8
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2021-03-12 09:31:09 +0200

    [Sort] Implement insertion sorting algorithm for the linked list.

---
+++ adv_sort/sort_insertion_linked_list.py
@@ -0,0 +1,25 @@
+# Sorts a linked list using the technique of the insertion sort.
+# A reference to the new ordered list is returned.
+
+def llInsertionSort(origList):
+	# Make sure the list contains at least one node
+	if origList is None:
+		return None
+
+	# Iterate through the original list
+	newList = None
+	while origList is not None:
+		# Assign a temp reference to the first node
+		curNode = origList
+
+		# Advance the original list reference to the next node
+		origList = origList.next
+
+		# Unlink the first node and insert into the new ordered list
+		curNode.next = None
+		newList = addToSortedList(newList, curNode)
+
+	# Return the list reference of the new ordered list
+	return newList
+
+
