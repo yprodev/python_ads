1c1ea1c57cbaef9019a0b0fb28475e09b1c83695
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2021-03-16 09:52:57 +0200

    [Tree] Implement binary search tree search method.

--- tree/binary_search_tree.py
+++ tree/binary_search_tree.py
@@ -12,6 +12,29 @@ class BSTMap:
 	def __iter__(self):
 		return _BSTMapIterator(self._root)
 
+	# Determines if the map contains the given key
+	def __contains__(self, key):
+		return self._bstSearch(self._root, key) is not None
+
+	# Returns the value associated with the key
+	def valueOf(self, key):
+		node = self._bstSearch(self._root, key)
+		assert node is not None, "Invalid map key"
+
+		return node.value
+
+	# Helper method that recursively searches the tree for a target key
+	def _bstSearch(self, subtree, target):
+		if subtree is None: # Base case
+			return None
+		elif target < subtree.key: # target is left of the subtree root
+			return self._bstSearch(subtree.left)
+		elif target > subtree.key: # target is right of the subtree root
+			return self._bstSearch(subtree.right)
+		else: # Base case
+			return subtree
+
+
 # Storage class for the binary search tree nodes of the map
 class _BSTMapNode:
 	def __init__(self, key, value):
