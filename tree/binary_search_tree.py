8782d21d3e8b7aee7c5f56598ce7a0f2335ca0c5
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2021-03-17 08:49:26 +0200

    [Tree] Implement BST remove method.

--- tree/binary_search_tree.py
+++ tree/binary_search_tree.py
@@ -68,6 +68,50 @@ class BSTMap:
 
 		return subtree
 
+	# Removes the map entry associated with the give key
+	def remove(self, key):
+		assert key in self, "Invalid map key"
+		self._root = self._bstRemove(self._root, key)
+		self._size -= 1
+
+	# Helper method that removes an existing item recursively
+	def _bstRemove(self, subtree, target):
+		# Search for the item in the tree
+		if subtree is None:
+			return subtree
+		elif target < subtree.key:
+			subtree.left = self._bstRemove(subtree.left, target)
+
+			return subtree
+		elif target > subtree.key:
+			subtree.right = self._bstRemove(subtree.right, target)
+
+			return subtree
+		# We found the node containing the item
+		else:
+			if subtree.left is None and subtree.right is None:
+				return None
+			elif subtree.left is None or subtree.right is None:
+				if subtree.left is not None:
+					return subtree.left
+				else:
+					return subtree.right
+			else:
+				successor = self._bstMinimum(subtree.right)
+				subtree.key = successor.key
+				subtree.value = successor.value
+				subtree.right = self._bstRemove(subtree.right, successor.key)
+
+				return subtree
+
+
+
+
+
+
+
+
+
 
 # Storage class for the binary search tree nodes of the map
 class _BSTMapNode:
